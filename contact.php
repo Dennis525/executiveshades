75d005104624ce69b6304368425f64c483d3f534
Dennis525 <dennodhia83@gmail.com>
2023-12-27 09:55:11 +0300

    first commit

---
+++ contact.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta
+      name="description"
+      content="First In Fashion Shades is the leading Business Offering Best Car Shade Nets and Installation at affordable price. Besides ot also offers tents services"
+    />
+    <!-- icon -->
+    <link rel="icon" href="assets/images/20.jpg" type="image/x-icon" />
+    <title>First In Fashion Shades - Kenya | Contact Us Page</title>
+    <!-- google fonts -->
+    <link
+      href="//fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600;700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+
+  <body>
+    <!--/Header-->
+    <header id="site-header" class="fixed-top">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light stroke py-lg-0">
+          <h1>
+            <a class="navbar-brand" href="index.php">
+              Executive<span class="sub-color"> Shades & Tents</span>
+            </a>
+          </h1>
+          <button
+            class="navbar-toggler collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+            <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav mx-lg-auto my-2 my-lg-0 navbar-nav-scroll">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="services.php">Services</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active" href="contact.php">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <!-- toggle switch for light and dark theme -->
+          <div class="mobile-position">
+            <nav class="navigation">
+              <div class="theme-switch-wrapper">
+                <!-- <label class="theme-switch" for="checkbox">
+                  <input type="checkbox" id="checkbox" />
+                  <div class="mode-container">
+                    <i class="gg-sun"></i>
+                    <i class="gg-moon"></i>
+                  </div>
+                </label> -->
+              </div>
+            </nav>
+          </div>
+          <!-- //toggle switch for light and dark theme -->
+        </nav>
+      </div>
+    </header>
+    <!--//Header-->
+    <!--/inner-page-->
+    <div class="inner-banner py-5">
+      <section class="w3l-breadcrumb text-left py-sm-5">
+        <div class="container">
+          <div class="w3breadcrumb-gids">
+            <div class="w3breadcrumb-left text-left">
+              <h2 class="inner-w3-title mt-sm-5 mt-4">Contact Us</h2>
+            </div>
+            <div class="w3breadcrumb-right">
+              <ul class="breadcrumbs-custom-path">
+                <li><a href="index.php">Home</a></li>
+                <li class="active">
+                  <span class="fas fa-angle-double-right mx-2"></span> Contact
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <!--//inner-page-->
+    <!-- contacts-5-grid -->
+    <div class="w3l-contact-10 py-5" id="contact">
+      <div class="form-41-mian pt-lg-4 pt-md-3 pb-lg-4">
+        <div class="container">
+          <div class="heading text-center mx-auto">
+            <h5 class="title-subw3hny text-center">Contact Us</h5>
+            <h3 class="title-w3l">
+              Got any <span class="inn-text">Questions? </span>
+            </h3>
+          </div>
+
+          <div class="contacts-5-grid-main mt-5">
+            <div class="contacts-5-grid">
+              <div class="map-content-5">
+                <div class="d-grid grid-col-2 justify-content-center">
+                  <div class="contact-type">
+                    <div class="address-grid">
+                      <span class="fas fa-map-marked-alt"></span>
+                      <h6>Address</h6>
+                      <p>Hawaki plaza along Digo Road</p>
+                    </div>
+                    <div class="address-grid">
+                      <span class="fas fa-envelope-open-text"></span>
+                      <h6>Email</h6>
+                      <a href="mailto:info@executiveshades.co.ke" class="link1"
+                        >info@executiveshades.co.ke</a
+                      >
+                    </div>
+                    <div class="address-grid">
+                      <span class="fas fa-phone-alt"></span>
+                      <h6>Phone</h6>
+                      <a href="tel:+254 700 254065" class="link1"
+                        >+254 700 254065</a
+                      >
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="form-inner-cont mt-5">
+            <form
+              action="https://formspree.io/f/xdovvklp"
+              method="post"
+              class="signin-form"
+            >
+              <div class="form-grids">
+                <div class="form-input">
+                  <input
+                    type="text"
+                    name="w3lName"
+                    id="w3lName"
+                    placeholder="Enter your name *"
+                    required=""
+                  />
+                </div>
+                <div class="form-input">
+                  <input
+                    type="text"
+                    name="w3lSubject"
+                    id="w3lSubject"
+                    placeholder="Enter subject "
+                    required
+                  />
+                </div>
+                <div class="form-input">
+                  <input
+                    type="email"
+                    name="w3lSender"
+                    id="w3lSender"
+                    placeholder="Enter your email *"
+                    required
+                  />
+                </div>
+                <div class="form-input">
+                  <input
+                    type="text"
+                    name="w3lPhone"
+                    id="w3lPhone"
+                    placeholder="Enter your Phone Number *"
+                    required
+                  />
+                </div>
+              </div>
+              <div class="form-input">
+                <textarea
+                  name="w3lMessage"
+                  id="w3lMessage"
+                  placeholder="Type your query here"
+                  required=""
+                ></textarea>
+              </div>
+              <div class="w3-submit text-right">
+                <button class="btn btn-style btn-primary">
+                  Send Message <i class="fas fa-paper-plane ms-2"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <!-- //contacts-5-grid -->
+    </div>
+
+    <div class="contacts-sub-5">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.8157334253124!2d36.837153739473685!3d-1.2844913430907656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1132f8122931%3A0x7a570d8a4f2d4cd4!2sHawaki%20Plaza!5e0!3m2!1sen!2ske!4v1684745939175!5m2!1sen!2ske"
+        width="600"
+        height="450"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+    <!--/footer-9-->
+    <footer class="w3l-footer9">
+      <section class="footer-inner-main py-5">
+        <div class="container py-md-4">
+          <div class="right-side">
+            <div class="row footer-hny-grids sub-columns">
+              <div class="col-lg-3 sub-one-left">
+                <h6>About</h6>
+                <p class="footer-phny pe-lg-5">
+                  First In Fashion Shades offer a variety of tents and shades
+                  makings and installation services
+                </p>
+                <div class="columns-2 mt-4 pt-lg-2">
+                  <ul class="social">
+                    <li>
+                      <a
+                        href="https://www.facebook.com/profile.php?id=100081861385256&mibextid=ZbWKwL"
+                        ><span class="fab fa-facebook-f"></span
+                      ></a>
+                    </li>
+                    <li>
+                      <a href="https://instagram.com/opiyo2278?igshid=MzNlNGNkZWQ4Mg=="
+                        ><span class="fab fa-instagram"></span
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-2 sub-two-right">
+                <h6>Services</h6>
+                <ul>
+                  <li>
+                    <a href="#processing"
+                      ><i class="fas fa-angle-right"></i> Tent Making</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#research"
+                      ><i class="fas fa-angle-right"></i> Shade net
+                      installation</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#metal"
+                      ><i class="fas fa-angle-right"></i> Garden Umbrella</a
+                    >
+                  </li>
+                  <li>
+                    <a href="#metal"
+                      ><i class="fas fa-angle-right"></i> Artifical Grass</a
+                    >
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-lg-3 sub-one-left ps-lg-5">
+                <h6>Contact Info</h6>
+                <ul>
+                  <li>
+                    <a href="tel:+"
+                      ><i class="fas fa-angle-right">Phone Number:</i> +254 700
+                      254065</a
+                    >
+                  </li>
+                  <li>
+                    <a href="mailto:info@executiveshades.co.ke"
+                      ><i class="fas fa-angle-right">Email:</i>
+                      info@executiveshades.co.ke</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="below-section mt-5 pt-lg-3">
+            <div class="copyright-footer">
+              <div class="columns text-left">
+                <p>
+                  &copy; 2023 | Executive Shades & Tents | Design by
+                  <a href="whatsapp://send?phone=254759440518" target="_blank">
+                    Dennis</a
+                  >
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a
+          class="whatsapp-link"
+          aria-label="Chat on WhatsApp"
+          href="whatsapp://send?phone=254700254065"
+          target="_blank"
+        >
+          <img alt="Chat on WhatsApp" src="assets/images/icons8-whatsapp.svg" />
+        </a>
+      </section>
+
+      <!-- Js scripts -->
+      <!-- move top -->
+      <button onclick="topFunction()" id="movetop" title="Go to top">
+        <span class="fas fa-level-up-alt" aria-hidden="true"></span>
+      </button>
+      <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function () {
+          scrollFunction();
+        };
+
+        function scrollFunction() {
+          if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+          ) {
+            document.getElementById("movetop").style.display = "block";
+          } else {
+            document.getElementById("movetop").style.display = "none";
+          }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+          document.body.scrollTop = 0;
+          document.documentElement.scrollTop = 0;
+        }
+      </script>
+      <!-- //move top -->
+    </footer>
+    <!--//footer-9 -->
+
+    <!-- Template JavaScript -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/theme-change.js"></script>
+    <!-- stats number counter-->
+    <script src="assets/js/jquery.waypoints.min.js"></script>
+    <script src="assets/js/jquery.countup.js"></script>
+    <script>
+      $(".counter").countUp();
+    </script>
+    <!--/search-->
+    <script src="assets/js/modernizr.custom.js"></script>
+    <script src="assets/js/classie.js"></script>
+    <script src="assets/js/demo1.js"></script>
+    <!--//search-->
+    <!-- MENU-JS -->
+    <script>
+      $(window).on("scroll", function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 80) {
+          $("#site-header").addClass("nav-fixed");
+        } else {
+          $("#site-header").removeClass("nav-fixed");
+        }
+      });
+
+      //Main navigation Active Class Add Remove
+      $(".navbar-toggler").on("click", function () {
+        $("header").toggleClass("active");
+      });
+      $(document).on("ready", function () {
+        if ($(window).width() > 991) {
+          $("header").removeClass("active");
+        }
+        $(window).on("resize", function () {
+          if ($(window).width() > 991) {
+            $("header").removeClass("active");
+          }
+        });
+      });
+    </script>
+    <!-- //MENU-JS -->
+
+    <!-- disable body scroll which navbar is in active -->
+    <script>
+      $(function () {
+        $(".navbar-toggler").click(function () {
+          $("body").toggleClass("noscroll");
+        });
+      });
+    </script>
+    <!-- //disable body scroll which navbar is in active -->
+    <!-- //bootstrap -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
